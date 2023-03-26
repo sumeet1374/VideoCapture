673edb498c232ba457d186314cc63396c5fed7c1
sumeetdeshmukh <sumeet_d@hotmail.com>
2023-03-26 18:03:10 +0530

    very basic video capturing code added.

--- src/app/videocapture/videocapture.component.ts
+++ src/app/videocapture/videocapture.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, NgZone, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-videocapture',
   templateUrl: './videocapture.component.html',
   styleUrls: ['./videocapture.component.css']
 })
-export class VideocaptureComponent {
+export class VideocaptureComponent  {
+  @ViewChild("videorecord")
+  canvas:ElementRef | undefined;
 
+   constructor(private zone:NgZone){
+
+   }
+
+    recordVideo(){
+    const recordedChunks:any[] = [];
+    this.zone.runOutsideAngular(async ()=> {
+      let compRef = this.canvas?.nativeElement;
+      const stream =  await navigator.mediaDevices.getUserMedia({ audio: true, video: true });//this.canvas?.nativeElement.captureStream(25);
+      //const options = { mimeType: "video/webm; codecs=vp9" };
+    const mediaRecorder = new MediaRecorder(stream);
+
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start();
+    function handleDataAvailable(event:any) {
+      console.log("data-available");
+      if (event.data.size > 0) {
+        recordedChunks.push(event.data);
+        console.log(recordedChunks);
+        compRef.src = URL.createObjectURL(new Blob(recordedChunks, { type: mediaRecorder.mimeType }));
+        //sdownload();
+      } else {
+        // …
+      }
+    }
+    
+    
+    // demo: to download after 9sec
+    setTimeout(() => {
+      console.log("stopping");
+      mediaRecorder.stop();
+      stream.getTracks().forEach(track => track.stop());
+    }, 9000);
+    });
+   }
 }
